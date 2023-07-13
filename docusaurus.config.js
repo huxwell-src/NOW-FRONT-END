442891a4879f99beab80fea497caa6b5fbaa07af
huxwell.src <niko2000gb@gmail.com>
2023-07-13 14:07:27 -0400

    vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NOW',
+  title: 'now',
   tagline: 'Pide y entregamos',
   favicon: 'img/logo.png',
 

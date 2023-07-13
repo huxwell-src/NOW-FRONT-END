6e62e28fe8c37afb22e55945373e0c4dfd92022a
huxwell.src <niko2000gb@gmail.com>
2023-07-13 01:13:09 -0400

    sad

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NOW!',
-  tagline: 'Dinosaurs are cool',
+  title: 'NOW',
+  tagline: 'Pide y entregamos',
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
